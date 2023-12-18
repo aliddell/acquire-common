9a7476db90b35c017ae52eb4353c48ee58aec7a1
Alan Liddell <aliddell@chanzuckerberg.com>
2023-12-18 16:59:18 -0500

    Move CMake files from subdirectories to a single cmake directory.

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -1,4 +1,4 @@
-option(ACQUIRE_MSVC_USE_MD "When ON, prefer linking to the Multhreaded DLL Microsoft C Runtime library (use /MD)")
+option(ACQUIRE_MSVC_USE_MD "When ON, prefer linking to the Multithreaded DLL Microsoft C Runtime library (use /MD)")
 
 if(ACQUIRE_MSVC_USE_MD)
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
